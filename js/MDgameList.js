fe2350e74485ae7279dd9e02f439b9cfcdb14679
twn1252839 <107757895+twn1252839@users.noreply.github.com>
2023-01-03 22:54:35 +0800

    MDgameList

--- js/MDgameList.js
+++ js/MDgameList.js
@@ -20,6 +20,10 @@ $(".MDgameList1 img").click(function () {
     "src",
     "./img/手機index_img/game/MDgameList/6.png"
   );
+  $(".MDgameList7 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/7.png"
+  );
 });
 $(".MDgameList2 img").click(function () {
   $(this).attr("src", "./img/手機index_img/game/MDgameList/List2.png");
@@ -42,6 +46,10 @@ $(".MDgameList2 img").click(function () {
   $(".MDgameList6 img").attr(
     "src",
     "./img/手機index_img/game/MDgameList/6.png"
+  );
+  $(".MDgameList7 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/7.png"
   );
 });
 $(".MDgameList3 img").click(function () {
@@ -66,6 +74,10 @@ $(".MDgameList3 img").click(function () {
     "src",
     "./img/手機index_img/game/MDgameList/6.png"
   );
+  $(".MDgameList7 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/7.png"
+  );
 });
 $(".MDgameList4 img").click(function () {
   $(this).attr("src", "./img/手機index_img/game/MDgameList/List4.png");
@@ -88,6 +100,10 @@ $(".MDgameList4 img").click(function () {
   $(".MDgameList6 img").attr(
     "src",
     "./img/手機index_img/game/MDgameList/6.png"
+  );
+  $(".MDgameList7 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/7.png"
   );
 });
 $(".MDgameList5 img").click(function () {
@@ -112,6 +128,10 @@ $(".MDgameList5 img").click(function () {
     "src",
     "./img/手機index_img/game/MDgameList/6.png"
   );
+  $(".MDgameList7 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/7.png"
+  );
 });
 $(".MDgameList6 img").click(function () {
   $(this).attr("src", "./img/手機index_img/game/MDgameList/List6.png");
@@ -134,5 +154,36 @@ $(".MDgameList6 img").click(function () {
   $(".MDgameList5 img").attr(
     "src",
     "./img/手機index_img/game/MDgameList/5.png"
+  );
+  $(".MDgameList7 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/7.png"
+  );
+});
+$(".MDgameList7 img").click(function () {
+  $(this).attr("src", "./img/手機index_img/game/MDgameList/List7.png");
+  $(".MDgameList1 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/1.png"
+  );
+  $(".MDgameList2 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/2.png"
+  );
+  $(".MDgameList3 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/3.png"
+  );
+  $(".MDgameList4 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/4.png"
+  );
+  $(".MDgameList5 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/5.png"
+  );
+  $(".MDgameList6 img").attr(
+    "src",
+    "./img/手機index_img/game/MDgameList/6.png"
   );
 });
--- js/gameList.js
+++ js/gameList.js
@@ -1,48 +1,12 @@
-$(".gameList1 img").click(function () {
-  $(this).attr("src", "./img/桌機index_img/GAME/gameList/List1.png");
-  $(".gameList2 img").attr("src", "./img/桌機index_img/GAME/gameList/2.png");
-  $(".gameList3 img").attr("src", "./img/桌機index_img/GAME/gameList/3.png");
-  $(".gameList4 img").attr("src", "./img/桌機index_img/GAME/gameList/4.png");
-  $(".gameList5 img").attr("src", "./img/桌機index_img/GAME/gameList/5.png");
-  $(".gameList6 img").attr("src", "./img/桌機index_img/GAME/gameList/6.png");
-});
-$(".gameList2 img").click(function () {
-  $(this).attr("src", "./img/桌機index_img/GAME/gameList/List2.png");
-  $(".gameList1 img").attr("src", "./img/桌機index_img/GAME/gameList/1.png");
-  $(".gameList3 img").attr("src", "./img/桌機index_img/GAME/gameList/3.png");
-  $(".gameList4 img").attr("src", "./img/桌機index_img/GAME/gameList/4.png");
-  $(".gameList5 img").attr("src", "./img/桌機index_img/GAME/gameList/5.png");
-  $(".gameList6 img").attr("src", "./img/桌機index_img/GAME/gameList/6.png");
-});
-$(".gameList3 img").click(function () {
-  $(this).attr("src", "./img/桌機index_img/GAME/gameList/List3.png");
-  $(".gameList1 img").attr("src", "./img/桌機index_img/GAME/gameList/1.png");
-  $(".gameList2 img").attr("src", "./img/桌機index_img/GAME/gameList/2.png");
-  $(".gameList4 img").attr("src", "./img/桌機index_img/GAME/gameList/4.png");
-  $(".gameList5 img").attr("src", "./img/桌機index_img/GAME/gameList/5.png");
-  $(".gameList6 img").attr("src", "./img/桌機index_img/GAME/gameList/6.png");
-});
-$(".gameList4 img").click(function () {
-  $(this).attr("src", "./img/桌機index_img/GAME/gameList/List4.png");
-  $(".gameList1 img").attr("src", "./img/桌機index_img/GAME/gameList/1.png");
-  $(".gameList2 img").attr("src", "./img/桌機index_img/GAME/gameList/2.png");
-  $(".gameList3 img").attr("src", "./img/桌機index_img/GAME/gameList/3.png");
-  $(".gameList5 img").attr("src", "./img/桌機index_img/GAME/gameList/5.png");
-  $(".gameList6 img").attr("src", "./img/桌機index_img/GAME/gameList/6.png");
-});
-$(".gameList5 img").click(function () {
-  $(this).attr("src", "./img/桌機index_img/GAME/gameList/List5.png");
-  $(".gameList1 img").attr("src", "./img/桌機index_img/GAME/gameList/1.png");
-  $(".gameList2 img").attr("src", "./img/桌機index_img/GAME/gameList/2.png");
-  $(".gameList3 img").attr("src", "./img/桌機index_img/GAME/gameList/3.png");
-  $(".gameList4 img").attr("src", "./img/桌機index_img/GAME/gameList/4.png");
-  $(".gameList6 img").attr("src", "./img/桌機index_img/GAME/gameList/6.png");
-});
-$(".gameList6 img").click(function () {
-  $(this).attr("src", "./img/桌機index_img/GAME/gameList/List6.png");
-  $(".gameList1 img").attr("src", "./img/桌機index_img/GAME/gameList/1.png");
-  $(".gameList2 img").attr("src", "./img/桌機index_img/GAME/gameList/2.png");
-  $(".gameList3 img").attr("src", "./img/桌機index_img/GAME/gameList/3.png");
-  $(".gameList4 img").attr("src", "./img/桌機index_img/GAME/gameList/4.png");
-  $(".gameList5 img").attr("src", "./img/桌機index_img/GAME/gameList/5.png");
-});
+for (let i = 1; i < 8; i++) {
+  $(".gameList" + [i] + "img").click(function () {
+    $(this).attr(
+      "src",
+      "./img/桌機index_img/GAME/gameList/List" + [i] + ".png"
+    );
+    $(".gameList" + [i] + "img").attr(
+      "src",
+      "./img/桌機index_img/GAME/gameList/" + [i] + ".png"
+    );
+  });
+}
